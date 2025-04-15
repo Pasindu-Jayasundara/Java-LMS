6226617c95b766d546144bb5c2b6156146b02d36
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-04-15 13:50:29 +0530

    notice panel items, not sure this works... need to check files-> NoticeListItemModel.java CustomNoticeListModel.java NoticeListRenderer.java NoticeItem.java

---
+++ LMS-NEW/src/controller/NoticeListRenderer.java
@@ -0,0 +1,21 @@
+package controller;
+
+import model.NoticeListItemModel;
+import view.lecturer.panels.NoticeItem;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class NoticeListRenderer extends NoticeItem implements ListCellRenderer<NoticeListItemModel> {
+    @Override
+    public Component getListCellRendererComponent(
+            JList<? extends NoticeListItemModel> list,
+            NoticeListItemModel value, int index, boolean isSelected, boolean cellHasFocus) {
+
+        this.setNoticeTitle(value.getTitle());
+        this.setContent(value.getContent());
+        this.setPostTime(value.getDate());
+
+        return this;
+    }
+}
---
+++ LMS-NEW/src/model/NoticeListItemModel.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.io.Serializable;
+
+public class NoticeListItemModel implements Serializable {
+
+    private String title;
+    private String content;
+    private String date;
+
+    public NoticeListItemModel() {
+    }
+
+    public NoticeListItemModel(String title, String content, String date) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
