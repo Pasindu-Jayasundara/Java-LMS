1bdea8e9fc227e0ea05ec175411ee1af81788956
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-04-20 22:46:38 +0530

    notice panel ui update and profile validation

--- LMS-NEW/src/view/lecturer/panels/NoticePanel.java
+++ LMS-NEW/src/view/lecturer/panels/NoticePanel.java
@@ -14,6 +14,7 @@ public class NoticePanel extends JPanel{
 
     private JList<NoticeListItemModel> jList1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton jButton1;
 
     private void createUIComponents() {
         initComponents();
@@ -23,14 +24,22 @@ public class NoticePanel extends JPanel{
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new JList<NoticeListItemModel>();
+        jList1 = new javax.swing.JList<>();
+        jButton1 = new javax.swing.JButton();
 
-//        jList1.setModel(new AbstractListModel<NoticeListItemModel>() {
+//        jList1.setModel(new javax.swing.AbstractListModel<String>() {
 //            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
 //            public int getSize() { return strings.length; }
 //            public String getElementAt(int i) { return strings[i]; }
 //        });
         jScrollPane1.setViewportView(jList1);
+
+        jButton1.setText("Reload Notices");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -38,17 +47,27 @@ public class NoticePanel extends JPanel{
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(20, 20, 20)
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 893, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addComponent(jButton1)
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 893, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addContainerGap(23, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
-                                .addGap(23, 23, 23)
+                                .addGap(14, 14, 14)
+                                .addComponent(jButton1)
+                                .addGap(19, 19, 19)
                                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 439, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(22, Short.MAX_VALUE))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
+        // reload
+        loadNotices();
+    }
+
 
     private void loadNotices() {
         String query = "SELECT * FROM `notice` WHERE `status_status_id`=? ORDER BY `post_date` DESC";
@@ -56,6 +75,7 @@ public class NoticePanel extends JPanel{
 
         if (resultSet != null) {
             CustomNoticeListModel model = new CustomNoticeListModel();
+            model.removeAllElements();
             try {
                 while (resultSet.next()) {
                     String title = resultSet.getString("title");
